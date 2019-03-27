bd22b3b3f9d12d5c210f3cce67f635c06bb2f4d7
guopeng <guopeng@tf.com>
2019-03-27 11:06:11 +0800

    add chaptor-05-singleFileComponent & README

---
+++ chaptor-05-singleFileComponent/single-file-component/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+      <p>{{ msg }}</p>
+      <div>
+              <input type="text" v-model="info">
+              <button @click="handleClick">Add</button>
+      </div>
+      <ul>
+          <todo-item v-for="item in list" :key="item">
+            <template v-slot:item="itemProps">
+              <span :style="{fontSize: '20px', color: itemProps.checked ? 'red': 'blue'}">{{item}}</span>
+            </template>
+          </todo-item>
+      </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'app',
+  data() {
+      return {
+          msg: 'hello Vue.js',
+          info: '',
+          list: [],
+      }
+  },
+  methods: {
+      handleClick() {
+          this.list.push(this.info)
+          this.info = ''
+      }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style>
+
+</style>
